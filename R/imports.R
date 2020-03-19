3f7841e6660fc19b1dd771441ad55cbc640351b2
gestraud <pierre.gestraud@curie.fr>
2020-03-19 15:43:30 +0100

    update imports

--- R/imports.R
+++ R/imports.R
@@ -3,15 +3,16 @@
 #' @importFrom grDevices dev.off pdf
 #' @importFrom graphics box plot
 #' @importFrom utils read.table write.csv
-#' @importFrom shinydashboard dashboardPage
+#' @importFrom shinydashboard dashboardPage box
 #' @importFrom dplyr rename select mutate group_by ungroup full_join filter arrange case_when inner_join summarise 
 #' @importFrom tidyr gather separate
 #' @importFrom forcats fct_reorder
 #' @importFrom stringr str_remove
-#' @importFrom ggplot2 ggplot position_dodge scale_fill_viridis_c geom_boxplot geom_abline facet_grid facet_wrap geom_col geom_line coord_equal aes labs .data
+#' @importFrom ggplot2 ggplot position_dodge scale_fill_viridis_c geom_boxplot geom_abline facet_grid facet_wrap geom_col geom_line coord_equal aes labs .data vars ylab
 #' @importFrom ggridges geom_density_ridges
 #' @importFrom DT datatable
 #' @importFrom readr read_delim
+#' @importFrom magrittr %>%
 #' @import shinyjqui
 #' @import shinydashboard
 
